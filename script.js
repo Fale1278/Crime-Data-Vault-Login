37c5aa3a51057b860bdc842f5baa0c57893e0d2f
Fale King Nanmua <kingnanmuafale@gmail.com>
2023-07-14 04:37:06 -0700

    Update Login styles and Linked to data site

--- script.js
+++ script.js
@@ -1,5 +1,9 @@
-const button = document.getElementById('myButton')
+// const button = document.getElementById('myButton')
 
-button.addEventListener('click', () => {
-    window.location.href = 'https://fale1278.github.io/Crime-Data-Vault/'
-})
+// button.addEventListener('click', () => {
+//     window.location.href = 'https://crime-data-vault.vercel.app/'
+// })
+
+const handleLogin = () => {
+    window.location.href = 'https://crime-data-vault.vercel.app/'
+}
